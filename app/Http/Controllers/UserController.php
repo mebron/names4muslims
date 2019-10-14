58b27aa9d70bf78e1e4683a6bad9ffee2068f0a9
Mujeeb Mebron <mujeeb@mebron.com>
2019-10-14 18:29:37 +0530

    user update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,80 +20,77 @@ class UserController extends Controller
 		$user = Auth::user();
 		return view('user.index', compact('user'));
 	}
-    public function profile()
-    {
+	public function profile()
+	{ }
+	public function update_avatar(Request $request)
+	{
+		if ($request->hasFile('avatar')) {
+			$name = Auth::id() . '_' . $request->avatar->hashName();
+			$avatar = $request->avatar;
+			$path = storage_path('app/users/avatar/' . $name);
+			if (Image::make($avatar->getRealPath())->resize(300, 300, function ($constraint) {
+				$constraint->aspectRatio();
+			})->save($path)) {
+				$user = Auth::user();
+				$user->avatar = $name;
+				$user->save();
+				return back();
+			}
+		}
+	}
+	public function lists()
+	{
+		$lists = UserList::where('active', 1)->get();
+		SEOTools::setTitle('User created name lists');
+		SEOTools::setDescription("Find some cool names from our user's name lists");
+		return view('user.user_lists', compact('lists'));
+	}
+	public function addList(Request $request)
+	{
+		$user = $request->user();
 
-    }
-    public function update_avatar(Request $request)
-    {
-    	if($request->hasFile('avatar'))
-    	{
-    		$name = Auth::id().'_'.$request->avatar->hashName();
-    		$avatar = $request->avatar;
-    		$path = public_path('storage/users/avatar/' . $name);
-    		if(Image::make($avatar->getRealPath())->resize(300, 300,function ($constraint) {
-            $constraint->aspectRatio();
-        	})->save($path)){
-    			$user = Auth::user();
-    			$user->avatar = $name;
-    			$user->save();
-    			return back();
-        	}
-    	}
-    }
-    public function lists()
-    {
-    	$lists = UserList::where('active', 1)->get();
-			SEOTools::setTitle('User created name lists');
-			SEOTools::setDescription("Find some cool names from our user's name lists");
-    	return view('user.user_lists', compact('lists'));
-    }
-    public function addList(Request $request)
-    {
-    	$user = $request->user();
+		$this->validate($request, [
+			'title' => 'required',
+			'slug' => 'rquired'
+		]);
+		$user_list = new UserList;
+		$user_list->user_id = $user->id;
+		$user_list->title = $request->title;
+		$user_list->description = $request->description;
+		$user_list->save();
+		$list_id = $user_list->id;
 
-    		$this->validate($request,[
-    			'title' => 'required',
-    			'slug' => 'rquired'
-    			]);
-    		$user_list = New UserList;
-    		$user_list->user_id = $user->id;
-    		$user_list->title = $request->title;
-    		$user_list->description = $request->description;
-    		$user_list->save();
-    		$list_id = $user_list->id;
+		foreach (Favorite::where('user_id', $user->id)->cursor() as $favorite) {
+			$list_names = new UserListNames;
+			$list_names->name_id = $favorite->name_id;
+			$list_names->user_list_id = $list_id;
+			$list_names->save();
+		}
+		Alert::success('Your lists are published successfully', 'Thank you')->persistent("Ok");
+		return redirect('/user/lists');
+	}
+	public function list_view($id)
+	{
 
-    		foreach (Favorite::where('user_id', $user->id)->cursor() as $favorite){
-    			$list_names = New UserListNames;
-    			$list_names->name_id = $favorite->name_id;
-    			$list_names->user_list_id = $list_id;
-    			$list_names->save() ;
-    		}
-    		Alert::success('Your lists are published successfully','Thank you')->persistent("Ok");
-    		return redirect('/user/lists');
-    }
-    public function list_view($id)
-    {
+		$names = UserList::find($id)->lists;
+		$lit = UserList::find($id);
+		$data['title'] = $lit->title;
+		$data['author'] = User::find($lit->user_id)->name;
+		SEOTools::setTitle($lit->title);
+		SEOTools::setDescription("This Baby name lists are created by our user -" . $data['author']);
 
-    	$names = UserList::find($id)->lists;
-    	$lit = UserList::find($id);
-    	$data['title'] = $lit->title;
-			$data['author'] = User::find($lit->user_id)->name;
-			SEOTools::setTitle($lit->title);
-			SEOTools::setDescription("This Baby name lists are created by our user -". $data['author']);
+		return view('user.user_list_names', compact('names', 'data'));
+	}
+	public function collection($slug)
+	{
 
-    	return view('user.user_list_names',compact('names','data'));
-    }
-    public function collection($slug)
-    {
-
-        $list = UserList::where('slug',$slug)->firstOrFail();
-        $names = $list->lists;
-        //$lit = UserList::find($id);
-        $data['title'] = $list->title;
-				$data['author'] = User::find($list->user_id)->name;
-				SEOTools::setTitle($list->title);
-				SEOTools::setDescription("This Baby name lists are created by our user - ". $data['author']);
-        return view('user.user_list_names',compact('names','data'));
-    }
+		$list = UserList::where('slug', $slug)->firstOrFail();
+		$names = $list->lists;
+		//$lit = UserList::find($id);
+		$data['title'] = $list->title;
+		$data['author'] = User::find($list->user_id)->name;
+		SEOTools::setTitle($list->title);
+		SEOTools::setDescription("This Baby name lists are created by our user - " . $data['author']);
+		return view('user.user_list_names', compact('names', 'data'));
+	}
 }
