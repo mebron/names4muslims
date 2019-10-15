32a4752468df08a36029ef072314734d931699e5
Mujeeb Mebron <mujeeb@mebron.com>
2019-10-15 15:12:07 +0530

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,98 @@
 <?php
 
+use App\Permission;
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Ask for db migration refresh, default is no
+        if ($this->command->confirm('Do you wish to refresh migration before seeding, it will clear all old data ?')) {
+            // disable fk constrain check
+            // \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            // Call the php artisan migrate:refresh
+            $this->command->call('migrate:refresh');
+            $this->command->warn("Data cleared, starting from blank database.");
+            // enable back fk constrain check
+            // \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        }
+        // Seed the default permissions
+        $permissions = Permission::defaultPermissions();
+        foreach ($permissions as $perms) {
+            Permission::firstOrCreate(['name' => $perms, 'guard_name' => 'web']);
+        }
+        $this->command->info('Default Permissions added.');
+        // Confirm roles needed
+        if ($this->command->confirm('Create Roles for user, default is admin and user? [y|N]', true)) {
+            // Ask for roles from input
+            $input_roles = $this->command->ask('Enter roles in comma separate format.', 'Admin,User');
+            // Explode roles
+            $roles_array = explode(',', $input_roles);
+            // add roles
+            foreach ($roles_array as $role) {
+                $role = Role::firstOrCreate(['name' => trim($role), 'guard_name' => 'web']);
+                if ($role->name == 'Admin') {
+                    // assign all permissions
+                    $role->syncPermissions(Permission::all());
+                    $this->command->info('Admin granted all the permissions');
+                    $this->createAdmin();
+
+                } else {
+                    // for others by default only read access
+                    $role->syncPermissions(Permission::where('name', 'LIKE', 'view_%')->get());
+                }
+                // create one user for each role
+                //$this->createUser($role);
+            }
+            $this->command->info('Roles ' . $input_roles . ' added successfully');
+        } else {
+            Role::firstOrCreate(['name' => 'User']);
+            $this->command->info('Added only default user role.');
+        }
+
+    }
+    /**
+     * Create a user with given role
+     *
+     * @param $role
+     */
+    private function createUser($role)
+    {
+        $user = factory(User::class)->create();
+        $user->assignRole($role->name);
+        if ($role->name == 'Admin') {
+            $this->command->info('Here is your admin details to login:');
+            $this->command->warn($user->email);
+            $this->command->warn('Password is "secret"');
+        }
+    }
+    /**
+     * Create a user with given role
+     *
+     * @param $role
+     */
+    private function createAdmin()
+    {
+        $input_user = $this->command->ask('Enter Admin Name');
+        $input_email = $this->command->ask('Enter Your email');
+        $input_password = $this->command->ask('Enter password');
+
+
+        $user = User::create([
+                'name'      => $input_user ,
+                'email'     => $input_email,
+                'password'  => bcrypt($input_password),                
+                ]);
+
+        $user->assignRole('Admin');
+        
     }
 }
