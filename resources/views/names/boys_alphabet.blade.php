d09baf9090e03e0d055405cfec629b5a3f30190d
Mujeeb Mebron <mujeeb@mebron.com>
2020-02-25 10:55:20 +0530

    fix

--- resources/views/names/boys_alphabet.blade.php
+++ resources/views/names/boys_alphabet.blade.php
@@ -28,34 +28,34 @@ data-ad-format="auto"></ins>
 @foreach($names as $name)
 
 <div class="row border mb-2 shadow-sm">
-    <div class="col-2 col-md-1 vertical-align position-relative {{ $name->gender === "Boy" ? "bg-boy" : "bg-girl" }}">
-        <a class="text-dark stretched-link" href="{{ url('/name',$name->slug)}}.html">{{ $name->name[0] }}</a></div>
-    <div class="col-11">
-        <div class="row">
-            <div class="col-12 pt-1 "><a class="name" href="{{ url('/name',$name->slug)}}.html">{{ $name->name }}</a>
-            </div>
+    <div class="col-2 col-md-1 vertical-align position-relative bg-boy">
+      <a class="text-boy stretched-link" href="#" data-toggle="modal" data-target="#exampleModalCenter"><h3 class="p-0">{{ $name->name[0] }}</h3></a></div>
+    <div class="col-10 col-md-11">
+      <div class="row">
+        <div class="col-12 pt-1 "><a class="name" href="{{ url('/name',$name->slug)}}.html">{{ $name->name }}</a>
         </div>
-        <div class="row">
-            <div class="col-10"><span
-                    class="font-weight-light {{ $name->gender === "Boy" ? "blue-link" : "text-girl" }}">{{ $name->gender }}</span>
-            </div>
-            <div class="col-2 text-right" id="fav">
-                @if (( Session::get('favorites')) && ($key = array_search($name->id, Session::get('favorites'))) !==
-                false)
-                <span class="text-warning" title="Shortlisted"><i class="fas fa-star"></i></span>
-                @else
-                <span title="Add to Favorites" id="{{ $name->id }}" class="{{ Auth::check() ? "myfav" : "fav" }}"><i
-                        class="far fa-star"></i></span>
-                @endif
-            </div>
+      </div>
+      <div class="row">
+        <div class="col-10"><span
+            class="font-weight-light {{ $name->gender === "Boy" ? "blue-link" : "text-girl" }}">{{ $name->gender }}</span>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <p class="font-weight-light">{{ strip_tags($name->meaning) }}</p>
-            </div>
+        <div class="col-2 text-right" id="fav">
+          @if (( Session::get('favorites')) && ($key = array_search($name->id, Session::get('favorites'))) !==
+          false)
+          <span class="text-warning" title="Shortlisted"><i class="fas fa-star"></i></span>
+          @else
+          <span title="Add to Favorites" id="{{ $name->id }}" class="{{ Auth::check() ? "myfav" : "fav" }}"><i
+              class="far fa-star"></i></span>
+          @endif
         </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <p class="font-weight-light">{{ strip_tags($name->meaning) }}</p>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 
 @if($loop->index == 11)
 <div class="row">
